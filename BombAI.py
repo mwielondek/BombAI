df66d0c602fdee01422175027bd51c84fc7cf09b
Milosz <miloszw@kth.se>
2013-05-17 18:04:39 +0200

    Fixade safelist som behöll gamla värden.

--- BombAI.py
+++ BombAI.py
@@ -91,26 +91,26 @@ def get_possible_moves(loc, board, bombs):
 def bomb_at(loc, bombs):
     return find(lambda bomb: bomb.loc == loc, bombs)
     
-def get_free_tiles(board, bombs):
-    tiles = set()
-    for y,row in enumerate(board.board):
-        for x,_ in enumerate(row):
-            loc = Location(x,y)
-            if(board.is_floor(loc) and not bomb_at(loc, bombs)):
-                tiles.add(loc)
-    
-    return tiles
-    
-def get_closest(x, list):
-    closest = None
-    for loc in list:
-        log(len(x-loc))
-    return closest
+# def get_free_tiles(board, bombs):
+#     tiles = set()
+#     for y,row in enumerate(board.board):
+#         for x,_ in enumerate(row):
+#             loc = Location(x,y)
+#             if(board.is_floor(loc) and not bomb_at(loc, bombs)):
+#                 tiles.add(loc)
+#     
+#     return tiles
+#     
+# def get_closest(x, list):
+#     closest = None
+#     for loc in list:
+#         log(len(x-loc))
+#     return closest
     
 def get_best_move(player, board, bombs, blast_paths):
     pc = get_possible_moves(player.loc, board, bombs)
     safelist = get_safe_move((board, bombs, blast_paths), player.loc, pc)
-    log("safelist: %s"%safelist)
+    log(safelist)
     
     if not safelist:
         return None
@@ -119,6 +119,7 @@ def get_best_move(player, board, bombs, blast_paths):
     for moves in safelist:
         if len(moves) < len(best_move):
             best_move = moves
+    del safelist[:]
     return best_move
 
 def get_safe_move(comp, loc, possible_moves, safelist=[], move_history=[], deep=0):
@@ -129,12 +130,17 @@ def get_safe_move(comp, loc, possible_moves, safelist=[], move_history=[], deep=
             newloc = loc + RDIRECTIONS[move]
             move_history_branch.append(move)
             if loc_is_safe(newloc, blast_paths):
+                log("Appending safelist with %s"%move_history_branch)
                 safelist.append(move_history_branch)
+                if len(move_history_branch) <= 2:
+                    log("breaking")
+                    break
             pc = get_possible_moves(newloc, board, bombs)
             try:
                 pc.remove(DIRECTIONS[-(RDIRECTIONS[move])])
             except ValueError:
                 pass
+            log("Deep: %s - calling get safe now with move history:\n %s"%(deep,move_history_branch))
             get_safe_move(comp, newloc, pc, safelist, move_history_branch, deep+1)
     
     return safelist
